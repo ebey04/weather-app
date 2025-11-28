b49d8f5522607d60ae2aa444c222d1613c579d5b
Elizabeth Bey <ebey04@gmail.com>
2025-11-28 13:03:31 -0500

    feat: write async function to fetch weather from visual crossing

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+async function getWeather(location) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=LARM9ULEU5XM2GV3ASNMLFY9W`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return {
+        location: data.address,
+        date: data.days[0].datetime,
+        forecast: data.currentConditions.conditions,
+        temp: data.currentConditions.temp
+    };
+}
