7a7df1376bec0c102e7d9ebc1f155038b9ceb1b2
Elizabeth Bey <ebey04@gmail.com>
2025-11-28 13:16:15 -0500

    feat: pull in html elements into DOM for index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./style.css";
 import { getWeather } from "./api.js";
 
-
+const container = document.getElementById('container');
+const input = document.getElementById('locationInput');
+const button = document.getElementById('weatherBtn');
