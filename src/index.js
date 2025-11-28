7c9aaa584e963b5f49cab0b4b838d09e6507f0dd
Elizabeth Bey <ebey04@gmail.com>
2025-11-28 13:44:43 -0500

    feat: write event listener to input button to call getWeather()

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ import { getWeather } from "./api.js";
 const container = document.getElementById('container');
 const input = document.getElementById('locationInput');
 const button = document.getElementById('weatherBtn');
+
+button.addEventListener("click", async () => {
+    container.innerHTML = ""
+    
+    const location = input.value;
+    const data = await getWeather(location);
+
+    const address = document.createElement("h1");
+    address.textContent = data.location;
+
+    const date = document.createElement("p");
+    date.textContent = data.date;
+
+    const forecast = document.createElement("p");
+    forecast.textContent = data.forecast;
+
+    const temp = document.createElement('p');
+    temp.classList.add('temp');
+    temp.textContent = data.temp;
+
+    container.appendChild(address);
+    container.appendChild(date);
+    container.appendChild(forecast);
+    container.appendChild(temp);
+});
