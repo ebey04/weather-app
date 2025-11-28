f38933ba8f440dbf357120eba43bca5d27da9898
Elizabeth Bey <ebey04@gmail.com>
2025-11-28 11:19:54 -0500

    setup esLint and Prettier extensions

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js, prettier: prettierRecommended.plugins.prettier },
+    extends: ["js/recommended", prettierRecommended, prettierConfig],
+    languageOptions: { globals: globals.browser, sourceType: "module" },
+  },
+]);
