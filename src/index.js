4c343ade2b7cb478abff097da71e3d4a3e0c910c
Elizabeth Bey <ebey04@gmail.com>
2025-11-28 14:01:29 -0500

    feat: format location to caps, date to locale format, style temp to be larger

--- src/index.js
+++ src/index.js
@@ -2,27 +2,31 @@ import "./style.css";
 import { getWeather } from "./api.js";
 
 const container = document.getElementById('container');
-const input = document.getElementById('locationInput');
+const input = document.getElementById('input');
 const button = document.getElementById('weatherBtn');
 
 button.addEventListener("click", async () => {
     container.innerHTML = ""
-    
+
     const location = input.value;
     const data = await getWeather(location);
 
     const address = document.createElement("h1");
-    address.textContent = data.location;
+    address.textContent = data.location.toUpperCase();
 
     const date = document.createElement("p");
-    date.textContent = data.date;
+    date.textContent = new Date (data.date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
 
     const forecast = document.createElement("p");
     forecast.textContent = data.forecast;
 
     const temp = document.createElement('p');
     temp.classList.add('temp');
-    temp.textContent = data.temp;
+    temp.textContent = `${data.temp}°F`;
 
     container.appendChild(address);
     container.appendChild(date);
